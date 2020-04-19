149a7078f393208629631bfe71acbd29744cccd6
Andrew Ecurut Angura <andre.ecurut@gmail.com>
2020-04-20 03:18:39 +0400

    Update webScraper.py

--- webScraper.py
+++ webScraper.py
@@ -1,8 +1,10 @@
 # scraping a website and storing the information acquired in a CVS file or Excel
 # https://www.youtube.com/watch?v=ng2o98k983k&t=1370s
+#=============important Links==========================================#
 #https://github.com/Kristian-Roopnarine/Digital-Market-Place
 #https://github.com/drewblay/Personal-Finance-Django-App
 #https://github.com/TusharKapoor23/django-expense-tracker
+#=======================================================================
 # 06-Nov-2019
 from bs4 import BeautifulSoup
 import requests
